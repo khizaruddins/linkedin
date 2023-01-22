5f236d9ba5b2eff9c4a86715d0366125bbbb95cb
syed khizaruddin <syedkhizaruddin@syeds-MacBook-Air.local>
2023-01-22 23:54:08 +0530

    update css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ function Header() {
   return (
     <div className='header'>
         <div className="header__left">
-            <img src="https://cdn-icons-png.flaticon.com/512/2111/2111499.png" alt="linkedin logo" />
+            <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="linkedin logo" />
             <div className="header__search">
                 <img src="https://cdn-icons-png.flaticon.com/512/2811/2811806.png" alt="search icon" />
-                <input type="text" />
+                <input type="text" placeholder='Search'/>
             </div>
         </div>
         <div className="header__right"></div>
